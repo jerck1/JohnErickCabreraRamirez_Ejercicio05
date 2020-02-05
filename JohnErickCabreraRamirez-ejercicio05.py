4ee9efdf2a72f014d09b4c01009a2825836de145
jerck1 <johncafre@gmail.com>
2020-02-05 12:00:58 -0500

    pushing

--- JohnErickCabreraRamirez-ejercicio05.py
+++ JohnErickCabreraRamirez-ejercicio05.py
@@ -15,29 +15,14 @@ import matplotlib.pyplot as plt
 toss=np.array(["s","c","c","c","c"])
 
 
-# In[14]:
+# In[3]:
 
 
 def P(H):
     return 1
 
 
-# In[9]:
-
-
-#def P(event,data):
- #   p=np.count_nonzero(toss == event)/np.size(toss)
-  #  return p
-
-
-# In[109]:
-
-
-H=np.arange(0,1.1,0.1)
-print(H)
-
-
-# In[35]:
+# In[19]:
 
 
 def P_obs_H(nc,ns,H):
@@ -45,27 +30,53 @@ def P_obs_H(nc,ns,H):
     return P
 
 
-# In[36]:
+# In[33]:
 
 
-P_obs_H(np.count_nonzero(toss == "s"),np.count_nonzero(toss == "c"),H)
-
-
-# In[37]:
-
-
-def P_H_obs(nc,ns,H,toss):
-    p=P_obs_H(nc,ns,H)*P(H)/np.size(toss)
+def P_obs(nc,ns,H):
+    p=0
+    for i in range(len(H)):
+        p+=P_obs_H(nc,ns,H[i])
     return p
 
 
-# In[38]:
+# In[34]:
+
+
+def P_H_obs(nc,ns,H,toss):
+    p=P_obs_H(nc,ns,H)*P(H)/P_obs(nc,ns,H)
+    return p
+
+
+# In[47]:
+
+
+def x_max(y,H):
+    for i in range(len(y)-2):
+        if np.diff(np.log(y))[i]*np.diff(np.log(y))[i+1]<0 :
+            return (H[i]+H[i+1])/2, i
+
+
+# In[56]:
+
+
+def sigma(y,x_bar,pos):
+    return -np.diff(np.log(y))[pos]
+
+
+# In[57]:
+
+
+H=np.arange(0,1.1,0.01)
+
+
+# In[58]:
 
 
 y=P_H_obs(np.count_nonzero(toss == "s"),np.count_nonzero(toss == "c"),H,toss)
 
 
-# In[39]:
+# In[59]:
 
 
 plt.figure(figsize=(8,8))
@@ -74,16 +85,7 @@ plt.xlabel("H")
 plt.ylabel('P(H|obs)')
 
 
-# In[74]:
-
-
-def x_max(y,H):
-    for i in range(len(y)-2):
-        if np.diff(y)[i]*np.diff(y)[i+1]<0 :
-            return (H[i]+H[i+1])/2, i
-
-
-# In[77]:
+# In[60]:
 
 
 x_bar=x_max(y, H)[0]
@@ -91,14 +93,7 @@ pos=x_max(y, H)[1]
 print(x_bar)
 
 
-# In[78]:
-
-
-def sigma(y,x_bar,pos):
-    return -np.diff(np.diff(y))[pos]
-
-
-# In[97]:
+# In[61]:
 
 
 sig=sigma(y,x_bar,pos)
@@ -106,14 +101,14 @@ print(sigma(y,x_bar,pos))
 valor=[x_bar,"+/-",sig]
 
 
-# In[101]:
+# In[62]:
 
 
 def gaussian(x, mu, sig):
     return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 
 
-# In[107]:
+# In[63]:
 
 
 plt.figure(figsize=(8,8))
@@ -122,12 +117,6 @@ plt.plot(H,gaussian(H,x_bar,sig))
 plt.xlabel("H")
 plt.ylabel('P(H|obs)')
 plt.suptitle(valor, fontsize=20)
-
-
-# In[108]:
-
-
-plt.plot(H,gaussian(H,x_bar,sig))
 
 
 # In[ ]:
